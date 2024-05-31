e4ce2642c8535f1dd68c11e3160f1443b8f794ab
Jonghyeok <jhpmuscle@gmail.com>
2024-05-31 21:31:59 +0900

    fix: format files

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,4 +1,4 @@
-import useCountStore from "../store/useCountStore";
+import useCountStore from '../store/useCountStore';
 
 function Main() {
   const count = useCountStore((state) => state.count);
@@ -8,9 +8,11 @@ function Main() {
     <main className="main">
       <h1>메인 페이지</h1>
       <h4>{count}</h4>
-      <button onClick={increaseCount}>Count</button>
+      <button type="button" onClick={increaseCount}>
+        Count
+      </button>
     </main>
-  )
+  );
 }
 
 export default Main;
