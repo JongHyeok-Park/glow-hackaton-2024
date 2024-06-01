9ae21389dd2024f2f2d6d3ef00b69a70f4bf3e04
Jonghyeok <jhpmuscle@gmail.com>
2024-06-01 16:09:30 +0900

    refactor: remove uselss codes, move components to components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Main from './pages/Main';
-import Navbar from './component/navbar/Navbar';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Navbar />
