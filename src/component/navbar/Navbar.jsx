d23c0e2115f4271a41b4cddcb10b9f573e7482b3
gamza <aprille138@gmail.com>
2024-06-01 14:47:55 +0900

    fix: change tags in Navbar

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 import NavbarProfile from './NavbarProfile';
 
-function Navbar(props) {
+function Navbar() {
   return (
     <header className="header">
       <div className="header-wrapper">
-        <article className="header-menu-wrapper">
-          <span className="header-site-logo">기가 막힌 로고</span>
-          <nav className="header-category">카테고리 보기</nav>
-          <section className="header-search-wrapper">
-            <form className="header-search-box" action="" method="get">
-              <input className="search-txt" type="search" name="search-txt"></input>
-              <button className="search-btn" type="submit">
-                <img src="../src/assets/icon/search-icon.png" alt="검색"></img>
-              </button>
-            </form>
-          </section>
-        </article>
-        <div className="header-profile-wrapper">
-          <NavbarProfile />
-        </div>
+        <nav className="header-menu-wrapper">
+          <Link className="header-site-logo" to="/">
+            기가 막힌 로고
+          </Link>
+          <Link className="header-category" to="/#">
+            카테고리 보기
+          </Link>
+          <form className="header-search-box" action="" method="get">
+            <input className="search-txt" type="search" name="search-txt" />
+            <button className="search-btn" type="submit">
+              <img src="../src/assets/icon/search-icon.png" alt="검색" />
+            </button>
+          </form>
+        </nav>
+        <NavbarProfile />
       </div>
     </header>
   );
--- src/component/navbar/NavbarProfile.jsx
+++ src/component/navbar/NavbarProfile.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import './NavbarProfile.css';
 
-function NavbarProfile(props) {
+function NavbarProfile() {
   return (
-    <article className="profile">
-      <section className="profile-wrapper">
-        <img className="profile-image" src="https://avatars.githubusercontent.com/u/114932050?v=4" alt=""></img>
-        <div className="dowm-wrapper">
-          <img className="down-icon" src="../src/assets/icon/arrow-down.png" alt="더보기"></img>
-        </div>
-      </section>
-    </article>
+    <section className="profile">
+      <img className="profile-image" src="https://avatars.githubusercontent.com/u/114932050?v=4" alt="" />
+      <div className="dowm-wrapper">
+        <img className="down-icon" src="../src/assets/icon/arrow-down.png" alt="더보기" />
+      </div>
+    </section>
   );
 }
 
