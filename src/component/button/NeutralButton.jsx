ba1f8b7b15462c742c9ec30222378bb2fbbbc5fd
gamza <aprille138@gmail.com>
2024-06-01 09:44:54 +0900

    feat: buttons - primary and neutral, jsx and css

---
+++ src/component/button/NeutralButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NeutralButton.css';
+
+function NeutralButton(props) {
+  return (
+    <div className="neutral-btn-wrapper">
+      <button className="neutral-btn">버튼</button>
+    </div>
+  );
+}
+
+export default NeutralButton;
---
+++ src/component/button/PrimaryButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PrimaryButton.css';
+
+function PrimaryButton(props) {
+  return (
+    <div className="primary-btn-wrapper">
+      <button className="primary-btn">버튼</button>
+    </div>
+  );
+}
+
+export default PrimaryButton;
