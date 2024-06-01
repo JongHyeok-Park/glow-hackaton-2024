e9854e2980f26ab188175e025614ea2cd6f36478
gamza <aprille138@gmail.com>
2024-06-01 15:34:31 +0900

    feat: add navbar at App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Main from './pages/Main'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import Navbar from './component/navbar/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <header>
-        상단 메뉴
-      </header>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Main />}/>
+        <Route path="/" element={<Main />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
